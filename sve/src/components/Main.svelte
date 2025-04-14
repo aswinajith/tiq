c00485f389460c11f88e20c9d4e462987f340ef7
Aswin Ajith <aswinajith@Aswins-MacBook-Air-2.local>
2025-04-14 14:53:53 +0530

    second page

--- sve/src/components/Main.svelte
+++ sve/src/components/Main.svelte
@@ -1,1 +1,18 @@
-<h1> This is brown</h1>
+<div class="flex relative bg-[#EDCA90]">
+	<div class="flex-col my-auto ml-30 mr-30">
+		<img class="w-500" src="../src/media/Frame_emblem.png" alt="tiq logo" />
+	</div>
+	<div class="flex-col ml-auto my-65">
+		<h1 class="relative text-6xl text-left font-[merriweather]">About TIQ</h1>
+		<h1 class="relative text-lg text-left mt-4 mr-10">
+			The Illuminati Quiz (TIQ), the annual inter collegiate quiz competition organised by Govt.
+			Model Engineering College is one of the largest student organised Quiz in South India, held in
+			memory of the late Sandeep Menon. Illuminati Quiz will also be open to all categories testing
+			the experience and mettle of working professionals against the young blood of quizzing. The
+			new normal paved way for quizeers accross the country to be part of this prestigious event.
+		</h1>
+		<a href="#About" target="_blank">
+			<h1 class="mt-4 text-sm font-extrabold text-[#532516]">Learn More &rarr;</h1>
+		</a>
+	</div>
+</div>
